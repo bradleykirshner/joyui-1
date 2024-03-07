a12b6eebeed0244b8d16157ac9aaafbe77ed96af
Bradley Kirshner <135774924+bradleykirshner@users.noreply.github.com>
2024-03-07 13:21:48 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -5,9 +5,10 @@ import Box from '@mui/joy/Box';
 export default function BasicButtons() {
   return (
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-      <Button>Button 1</Button>
+      <Button>Button</Button>
       <Button disabled>Disabled</Button>
       <Button loading>Loading</Button>
+      <Button>Click me</Button>
     </Box>
   );
 }
