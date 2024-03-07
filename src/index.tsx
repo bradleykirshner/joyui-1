7853de8aea385c4053db50b0cefb4625b3ff27da
Bradley Kirshner <135774924+bradleykirshner@users.noreply.github.com>
2024-03-07 14:13:16 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
+
 import Demo from './BasicButtons';
 import Demo2 from './Demo2';
-import StepSlider from './slider-component'
+import { StepSlider } from './slider-component'
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
