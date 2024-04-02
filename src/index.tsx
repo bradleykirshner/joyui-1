8d9d95e135398d095b72e0694e96a47751ba004e
Bradley Kirshner <135774924+bradleykirshner@users.noreply.github.com>
2024-04-02 12:30:09 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 
-import SignIn from './signin'; // Import the SignIn component
 import SignInPage from './signInPage'; // Import the SignInPage component
 import ColorPicker from './colorPicker'; // Import the ColorPicker component
 
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <CssVarsProvider>
-        <SignIn /> {/* Render the SignIn component */}
         <SignInPage /> {/* Render the SignInPage component */}
         <ColorPicker /> {/* Render the ColorPicker component */}
       </CssVarsProvider>
