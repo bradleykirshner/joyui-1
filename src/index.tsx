dec6fef93d7a7b1044a60c9467c7de26e9f24958
Bradley Kirshner <135774924+bradleykirshner@users.noreply.github.com>
2024-04-02 12:42:28 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,20 +7,21 @@ import ColorPicker from './colorPicker'; // Import the ColorPicker component
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-  <React.StrictMode>
-    <Routes>
-                    <Route path="/signinpage" element={<SignIn />} />
-                    <Route path="/colorpicker" element={<ColorPicker />} />
-                </Routes>
-                <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                    <Link to="/signinpage">Sign In Page</Link>
-                </div>
-                <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                    <Link to="colorpicker">Color Picker</Link>
-                </div>
-            </BrowserRouter>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+<React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/signinpage" element={<SignIn />} />
+            <Route path="/colorpicker" element={<ColorPicker />} />
+        </Routes>
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                <Link to="/signinpage">Sign In Page</Link>
+            </div>
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                <Link to="colorpicker">Color Picker</Link>
+            </div>
+    </BrowserRouter>
+</React.StrictMode>
+    );
 }
